56c59a11149b517240f45dff48db5656f9798adc
sdxu <xusd3@mail2.sysu.edu.cn>
2022-12-13 13:26:13 +0800

    解决找不到mapper

--- OJ-backend/src/main/java/com/coding/oj/OjBackendApplication.java
+++ OJ-backend/src/main/java/com/coding/oj/OjBackendApplication.java
@@ -1,5 +1,6 @@
 package com.coding.oj;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
