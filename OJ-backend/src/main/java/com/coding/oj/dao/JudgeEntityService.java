dfd2497622db2b6dccd61a91af2afe44b99676c1
许承灏 <1905698198@qq.com>
2022-12-17 20:15:19 +0800

    增加了获取评测结果的接口

--- OJ-backend/src/main/java/com/coding/oj/dao/JudgeEntityService.java
+++ OJ-backend/src/main/java/com/coding/oj/dao/JudgeEntityService.java
@@ -3,6 +3,8 @@ package com.coding.oj.dao;
 import com.coding.oj.common.result.CommonResult;
 import com.coding.oj.pojo.dto.SubmitJudgeDTO;
 import com.coding.oj.pojo.entity.Judge;
+import com.coding.oj.pojo.entity.JudgeCase;
+
 import java.util.List;
 
 
@@ -37,4 +39,7 @@ public interface JudgeEntityService {
 
     List<Judge> selectByParam(int userId, Long pid, Integer status, String language, String title);
 
+    Judge getSubmissionDetail(Long submitId);
+
+    List<JudgeCase> getAllcaseResult(Long submitId);
 }
--- OJ-backend/src/test/java/com/coding/oj/dao/JudgeCaseServiceTest.java
+++ OJ-backend/src/test/java/com/coding/oj/dao/JudgeCaseServiceTest.java
@@ -32,4 +32,5 @@ public class JudgeCaseServiceTest {
         boolean addCaseRes = judgeCaseService.saveBatch(allCaseResList);
         assertEquals(true,addCaseRes);
     }
+
 }
