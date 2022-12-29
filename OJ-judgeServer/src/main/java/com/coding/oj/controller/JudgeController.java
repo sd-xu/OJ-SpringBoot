6f1d6f2e1e40e6b2e206c789663115505b0e1c64
sdxu <xusd3@mail2.sysu.edu.cn>
2022-12-29 18:16:09 +0800

    json

--- OJ-judgeServer/src/main/java/com/coding/oj/controller/JudgeController.java
+++ OJ-judgeServer/src/main/java/com/coding/oj/controller/JudgeController.java
@@ -5,7 +5,9 @@ import com.coding.oj.common.Result;
 import com.coding.oj.pojo.dto.ToJudgeDTO;
 import com.coding.oj.pojo.entity.Judge;
 import com.coding.oj.service.JudgeService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,16 +25,25 @@ public class JudgeController {
     @Autowired
     private JudgeService judgeService;
 
-    @PostMapping(value = "/judge")
-    public Result submitProblemJudge(@RequestBody ToJudgeDTO toJudgeDTO) throws Exception {
+    @PostMapping(value = "/judge", produces = MediaType.APPLICATION_JSON_VALUE)
+    public JSONObject submitProblemJudge(@RequestBody ToJudgeDTO toJudgeDTO) throws Exception {
 
         Judge judge = toJudgeDTO.getJudge();
+        Result result = null;
+        JSONObject jsonObject = new JSONObject();
         if (judge == null || judge.getSubmitId() == null || judge.getUid() == null || judge.getPid() == null) {
-            return Result.successResponsemsg("调用参数错误！请检查您的调用参数！");
+            result = Result.successResponsemsg("调用参数错误！请检查您的调用参数！");
+            jsonObject.put("status", result.getStatus());
+            jsonObject.put("data", result.getData());
+            jsonObject.put("msg", result.getMsg());
+            return jsonObject;
         }
         judgeService.judge(judge);
-        return Result.successResponsemsg("判题机评测完成！");
-
+        result = Result.successResponsemsg("判题机评测完成！");
+        jsonObject.put("status", result.getStatus());
+        jsonObject.put("data", result.getData());
+        jsonObject.put("msg", result.getMsg());
+        return jsonObject;
     }
 
 }
